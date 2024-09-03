05283eb57c77c45ca4dd170f9bcca37ab3122e07
gustavo henrique <ghgoncalves430@gmail.com>
2024-09-03 17:08:19 -0300

    Commit inicial do projeto clonado

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const qrcode = require('qrcode-terminal')
 const { sessionFolderPath } = require('../config')
 const { sendErrorResponse } = require('../utils')
+const axios = require('axios')
 
 /**
  * Responds to ping request with 'pong'
@@ -36,6 +37,26 @@ const ping = async (req, res) => {
  * @returns {Promise<void>} - Promise that resolves once response is sent
  * @throws {Object} - Throws error if response fails
  */
+
+//const webhookUrl = 'https://webhook.site/3804d4b7-1cd1-4880-9c0a-4107316d5f71';
+const webhookUrl = 'https://clickwhats.clicki9.com.br/api/webhook/messaging?event=MESSAGE_RECEIVED';
+
+// const sendToWebhook = async (data) => {
+//   if (data.dataType === 'message') {
+//     return axios.post(webhookUrl, data);
+//   }
+// };
+
+const sendToWebhook = async (data) => {
+  if (data.dataType === 'message') {
+
+    const formData = new URLSearchParams();
+    formData.append('payload', JSON.stringify(data));
+
+    return axios.post(webhookUrl, formData);
+  }
+};
+
 const localCallbackExample = async (req, res) => {
   /*
     #swagger.tags = ['Various']
@@ -43,6 +64,7 @@ const localCallbackExample = async (req, res) => {
   try {
     const { dataType, data } = req.body
     if (dataType === 'qr') { qrcode.generate(data.qr, { small: true }) }
+    await sendToWebhook(req.body);
     fs.writeFile(`${sessionFolderPath}/message_log.txt`, `${JSON.stringify(req.body)}\r\n`, { flag: 'a+' }, _ => _)
     res.json({ success: true })
   } catch (error) {
